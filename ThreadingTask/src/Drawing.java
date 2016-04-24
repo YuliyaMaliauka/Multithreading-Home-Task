ab446bf8fc16e9a7c70c8e1797d929393e3db2f9
YuliyaMaliauka <mao85yulimali@gmail.com>
2016-04-24 18:15:01 +0200

    Update Drawing.java

--- ThreadingTask/src/Drawing.java
+++ ThreadingTask/src/Drawing.java
@@ -4,7 +4,6 @@ import draw.Pencil;
 
 public class Drawing {
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
 		Artist a = new Artist();
 		new Pencil(a);		
 		new Eraser(a);
