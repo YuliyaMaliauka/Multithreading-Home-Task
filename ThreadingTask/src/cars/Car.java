e606647063a9a30efbacf51eea3d56c4bcf8ce34
YuliyaMaliauka <mao85yulimali@gmail.com>
2016-04-24 18:11:03 +0200

    Update Car.java

--- ThreadingTask/src/cars/Car.java
+++ ThreadingTask/src/cars/Car.java
@@ -5,50 +5,43 @@ import java.util.logging.Logger;
 
 public class Car implements Runnable {
 	public static final long MAX_DISTANCE = 10000;        
-    public static volatile int countSeconds = 5000;
+    	public static volatile int countSeconds = 5000;
     
         private final Logger log = Logger.getLogger(Car.class.getName());
-        private long friction;
-        private long distance;
-        public String name;
-        public Thread t;
+        	private long friction;
+        	private long distance;
+        	public String name;
+        	public Thread t;
         
-        public Car(String name, long friction) {
-            this.name = name;
-            this.friction = friction;
-            t = new Thread(this, name);
-            log.info(name+" on start");
-            t.start();
-        }
-        public synchronized long getDistanse(){return distance;}
-        @Override
-        public void run() {
-        	
+        	public Car(String name, long friction) {
+            		this.name = name;
+            		this.friction = friction;
+            		t = new Thread(this, name);
+            		log.info(name+" on start");
+            		t.start();
+        	}
+        	public synchronized long getDistanse(){return distance;}
+        	@Override
+        	public void run() {
         	try {
-        		 if (friction<countSeconds){
-                     while (distance < MAX_DISTANCE) {               	 
-                	              		      			
-                         Thread.sleep(friction);
-                       
-                         distance += 100;
-                         System.out.println(name + " " + distance);
-                         
-                     }  }
-                     else{
-                    	 System.out.println(name + " Disqualify"); 
-                     }
-                	              	 
+        		if (friction<countSeconds){
+                		while (distance < MAX_DISTANCE) {               	 
+                        		Thread.sleep(friction);
+				 	distance += 100;
+                         		System.out.println(name + " " + distance);
+                     		}  
+        		}
+                     	else{
+                    	 	System.out.println(name + " Disqualify"); 
+                     	}
+        	} catch (InterruptedException ex ) {
+                 	log.log(Level.SEVERE, "Exception: ", ex);
+             	}
                  
-                     
-        		                  
-             } catch (InterruptedException ex ) {
-                 log.log(Level.SEVERE, "Exception: ", ex);
-             }
-                 
-             log.fine("some minor, debug message");
+             	log.fine("some minor, debug message");
          
-             if (log.isLoggable(Level.FINE)) {
-                 log.fine("Some CPU consuming message: ");
-             }            
+             	if (log.isLoggable(Level.FINE)) {
+                 	log.fine("Some CPU consuming message: ");
+             	}            
         }
 }
